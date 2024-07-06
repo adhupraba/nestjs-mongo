a8c5daaf4b6093a6e958ce33120185ae1ef9efc0
Adharsh <getmeadharsh@gmail.com>
2024-07-06 12:57:03 +0530

    added abstract entity repository to reduce code duplication for module repositories

--- src/users/users.repository.ts
+++ src/users/users.repository.ts
@@ -1,26 +1,12 @@
 import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { User, UserDoc } from "./schemas/user.schema";
-import { FilterQuery, Model } from "mongoose";
+import { Model } from "mongoose";
+import { EntityRepository } from "src/database/entity.repository";
 
 @Injectable()
-export class UsersRepository {
-  constructor(@InjectModel(User.name) private userModel: Model<UserDoc>) {}
-
-  async findOne(userFilterQuery: FilterQuery<User>): Promise<User> {
-    return this.userModel.findOne(userFilterQuery);
-  }
-
-  async find(userFilterQuery: FilterQuery<User>): Promise<User[]> {
-    return this.userModel.find(userFilterQuery);
-  }
-
-  async create(user: User): Promise<User> {
-    const newUser = new this.userModel(user);
-    return newUser.save();
-  }
-
-  async findOneAndUpdate(userFilterQuery: FilterQuery<User>, user: Partial<User>): Promise<User> {
-    return this.userModel.findOneAndUpdate(userFilterQuery, user, { new: true });
+export class UsersRepository extends EntityRepository<UserDoc> {
+  constructor(@InjectModel(User.name) userModel: Model<UserDoc>) {
+    super(userModel);
   }
 }
